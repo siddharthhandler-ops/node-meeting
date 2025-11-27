f33cbbf17e30e9b51f407a3e1b52b1f46c02ad6e
siddharthhandler-ops <siddharthhandler@gmail.com>
2025-11-27 16:58:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Meeting from "./models/meeting.js";
 import { Server } from "socket.io";
 
-const app = express();
+dotenv.config();
 
+const app = express();
 app.use(cors());
-app.get("/", (req, res) => {
-  res.send("Socket server is running ");
-});
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
+
+app.get("/", (req, res) => res.send("Socket server running with MongoDB"));
 
 const server = http.createServer(app);
 
@@ -22,20 +31,26 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("join-room", ({ roomId, name }) => {
+  socket.on("join-room", async ({ roomId, name }) => {
     socket.data.name = name;
     socket.join(roomId);
 
+    await Meeting.findOneAndUpdate(
+      { roomId },
+      {
+        $setOnInsert: { roomId },
+        $push: { users: { userId: socket.id, name } },
+      },
+      { upsert: true, new: true }
+    );
+
     const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
     const otherClients = clients.filter((id) => id !== socket.id);
+
     socket.emit("existing-users", otherClients);
+    socket.to(roomId).emit("user-joined", { userId: socket.id, name });
 
-    socket.to(roomId).emit("user-joined", {
-      userId: socket.id,
-      name,
-    });
-
-    console.log(`User ${socket.id} joined room ${roomId}`);
+    console.log(`User ${name} (${socket.id}) joined ${roomId}`);
   });
 
   socket.on("offer", ({ to, from, sdp }) => {
@@ -50,19 +65,29 @@ io.on("connection", (socket) => {
     io.to(to).emit("ice-candidate", { from, candidate });
   });
 
-  socket.on("disconnecting", () => {
+  socket.on("disconnecting", async () => {
     for (const roomId of socket.rooms) {
       if (roomId === socket.id) continue;
+
+      await Meeting.updateOne(
+        { roomId, "users.userId": socket.id },
+        { $set: { "users.$.leftAt": new Date() } }
+      );
+
       socket.to(roomId).emit("user-left", { userId: socket.id });
+      console.log(`User Left: ${socket.id}`);
     }
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    console.log("Disconnected:", socket.id);
   });
 });
 
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`Socket server running on port ${PORT}`);
+app.get("/meeting/:roomId", async (req, res) => {
+  const meeting = await Meeting.findOne({ roomId: req.params.roomId });
+  res.json(meeting || {});
 });
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
